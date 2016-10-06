6433a521a58df8fe41ba4f7d1524f76d74df7260
Anthony Licata <tamamaroro@gmail.com>
2016-10-06 12:14:01 -0400

    Added initial Unity Project

---
+++ Abs_Neon/Assets/NewAtlantis/Users/Scripts/anthony_licata_scripts/SynthController.cs
@@ -0,0 +1,201 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SynthController : MonoBehaviour {
+
+    public int noteLength = 1;
+    // 1 = 1/16, 2 = 1/8, 3 = 1/4, 4 = 1/2, 5 = 1
+    public int noteMaxTriggerSpace;
+    // likelihood that a note will be played on the next tick (according to noteTriggerResolution)
+    public int noteTriggerResolution;
+    // Tick to (possibly) trigger a note on
+    // 1 = 1/16, 2 = 1/8, 3 = 1/4, 4 = 1/2, 5 = 1
+    public int octaveRange;
+    // 1 = 1 octave, 2 = 2 mid/high, 3 = 3 low/mid/high
+    public float chordFrequency;
+    // likelihood that tne next note played will be a chord
+    public int ticksUntilPlay;
+    bool generateNewChord = true;
+    Chord currChord;
+
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if(ticksUntilPlay <= 0)
+        {
+            //play currChord
+        }
+	    if (generateNewChord)
+        {
+            int prevLength = noteLength;
+            //noteLength = Random.Range(1, 5);
+            ticksUntilPlay = prevLength + Random.Range(0, noteMaxTriggerSpace + 1);
+            Note rootNote = new Note(Random.Range(1, 13), Random.Range(1, octaveRange + 1), noteLength);
+            currChord = new Chord(rootNote, 3, 1);
+            generateNewChord = false;
+        }
+	}
+
+    public void proceed()
+    {
+        if(ticksUntilPlay > 0)
+        {
+            ticksUntilPlay--;
+        }
+        else
+        {
+            generateNewChord = true;
+        }
+    }
+}
+
+class Chord {
+
+    public Note[] notes;
+    public int chordForm;
+    // 1 = monad, 2 = dyad, 3 = triad, 4 = tetrad
+    public int chordHarmonicContent;
+    // 1 = Major, 2 = minor, 3 = augmented, 4 = diminished
+    // (in dyads these only reflect the major & minor third rn to match up with the triads)
+    // (sevenths) 5 = Minor major, 6 = dominant, 7 = augmented major, 8 = half-diminished
+
+    public Chord(Note rootNote, int chordForm, int chordHarmonicContent)
+    {
+        this.chordForm = chordForm;
+        notes = new Note[chordForm];
+        this.chordHarmonicContent = chordHarmonicContent;
+        notes[0] = rootNote;
+        if(chordForm > 1)
+        {
+            int third = rootNote.degree, octave = rootNote.octave;
+            switch (chordHarmonicContent)
+            {
+                case 1:
+                    third += 4;
+                    break;
+                case 2:
+                    third += 3;
+                    break;
+                case 3:
+                    third += 4;
+                    break;
+                case 4:
+                    third += 3;
+                    break;
+                case 5:
+                    third += 3;
+                    break;
+                case 6:
+                    third += 4;
+                    break;
+                case 7:
+                    third += 4;
+                    break;
+                case 8:
+                    third += 3;
+                    break;
+            }
+            if (third > 12)
+            {
+                octave += 1;
+                third %= 12;
+            }
+                notes[1] = new Note(third, octave, rootNote.length);
+            if (chordForm > 2)
+            {
+                int fifth = rootNote.degree;
+                octave = rootNote.octave;
+                switch (chordHarmonicContent)
+                {
+                    case 1:
+                        fifth += 7;
+                        break;
+                    case 2:
+                        fifth += 7;
+                        break;
+                    case 3:
+                        fifth += 8;
+                        break;
+                    case 4:
+                        fifth += 6;
+                        break;
+                    case 5:
+                        fifth += 7;
+                        break;
+                    case 6:
+                        fifth += 7;
+                        break;
+                    case 7:
+                        fifth += 7;
+                        break;
+                    case 8:
+                        fifth += 7;
+                        break;
+                }
+                if (fifth > 12)
+                {
+                    octave += 1;
+                    fifth %= 12;
+                }
+                notes[2] = new Note(fifth, octave, rootNote.length);
+                if (chordForm > 3)
+                {
+                    int seventh = rootNote.degree;
+                    octave = rootNote.octave;
+                    switch (chordHarmonicContent)
+                    {
+                        case 1:
+                            seventh += 11;
+                            break;
+                        case 2:
+                            seventh += 10;
+                            break;
+                        case 3:
+                            seventh += 11;
+                            break;
+                        case 4:
+                            seventh += 9;
+                            break;
+                        case 5:
+                            seventh += 11;
+                            break;
+                        case 6:
+                            seventh += 10;
+                            break;
+                        case 7:
+                            seventh += 11;
+                            break;
+                        case 8:
+                            seventh += 11;
+                            break;
+                    }
+                    if (seventh > 12)
+                    {
+                        octave += 1;
+                        seventh %= 12;
+                    }
+                    notes[3] = new Note(seventh, octave, rootNote.length);
+                }
+            }
+        }
+    }
+}
+
+class Note
+{
+    public int degree;
+    public int octave;
+    public int length;
+    public Note(int deg, int oct, int len)
+    {
+        this.degree = deg;
+        this.octave = oct;
+        this.length = len;
+    }
+    public Note() { }
+}
